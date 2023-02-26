95075da176be082a1cac6a3ee3bbb8d5afb2c08e
Ming Chang <mail@mingchang.tw>
2023-02-26 23:47:33 +0800

    properly save file content into memory.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 use std::{
     fs::{read_dir, File},
     net::SocketAddr,
-    sync::OnceLock,
+    sync::OnceLock, io::Read,
 };
 
 use axum::{
@@ -13,12 +13,13 @@ use axum::{
     routing::get,
     Router, Server,
 };
-use tokio::{fs::File as TokioFile, runtime::Handle};
+use tokio::runtime::Handle;
 use tokio_util::io::ReaderStream;
 
+#[derive(Debug)]
 struct FileCache {
     file_name: String,
-    file: File,
+    file: &'static[u8],
 }
 
 static FILES: OnceLock<Vec<FileCache>> = OnceLock::new();
@@ -30,9 +31,12 @@ async fn main() {
         let mut files = Vec::new();
         for path_result in paths {
             let path = path_result.unwrap();
-            let file = File::open(path.path()).unwrap();
+            let mut file = File::open(path.path()).unwrap();
             let file_name = path.file_name().into_string().unwrap();
-            let file_cache = FileCache { file_name, file };
+            let mut file_buffer = Vec::new();
+            file.read_to_end(&mut file_buffer);
+            let leaked_buffer = Vec::leak(file_buffer);
+            let file_cache = FileCache { file_name, file: leaked_buffer };
             files.push(file_cache);
         }
         FILES.set(files);
@@ -57,8 +61,7 @@ async fn sendfile_api(Path(req_filename): Path<String>) -> impl IntoResponse {
         .iter()
         .find(|file_cache| file_cache.file_name == req_filename)
         .unwrap();
-    let file = TokioFile::from_std(file_cache.file.try_clone().unwrap());
-    let reader_stream = ReaderStream::new(file);
+    let reader_stream = ReaderStream::new(file_cache.file);
     let body = StreamBody::new(reader_stream);
     let mut headers = HeaderMap::new();
     headers.append(
