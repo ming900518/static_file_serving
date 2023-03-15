369e3b35cbe96894fc3094a05271135c7582ebb2
Ming Chang <mail@mingchang.tw>
2023-03-15 13:18:31 +0800

    io-uring: add tokio_uring logic.

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,32 @@ use axum::{
     Router, Server,
 };
 use mimalloc::MiMalloc;
+use tokio_uring::fs::File;
 use std::net::SocketAddr;
-use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-#[tokio::main]
-async fn main() {
-    let router = Router::new()
-        .route("/sendfile/:req_filename", get(sendfile_api))
-        .into_make_service();
+fn main() {
+    tokio_uring::start(async {
+        let router = Router::new()
+            .route("/sendfile/:req_filename", get(sendfile_api))
+            .into_make_service();
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
-    println!("SSL disabled. Listening on {}", addr);
-    Server::bind(&addr)
-        .serve(router)
-        .await
-        .expect("Server startup failed.");
+        let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
+        println!("SSL disabled. Listening on {}", addr);
+        Server::bind(&addr)
+            .serve(router)
+            .await
+            .expect("Server startup failed.");
+    })
 }
 
 async fn sendfile_api(Path(req_filename): Path<String>) -> impl IntoResponse {
-    let file = File::open(format!("./assets/{}", req_filename)).await.expect("file not found");
+    let file = File::open(format!("./assets/{}", req_filename))
+        .await
+        .expect("file not found");
     let mut headers = HeaderMap::new();
     headers.append(
         header::CONTENT_TYPE,
