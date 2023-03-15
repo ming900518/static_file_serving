b90f7b153203fb2e959f6a8225dc8bff3d6c87d5
Ming Chang <mail@mingchang.tw>
2023-03-15 14:20:02 +0800

    update project for using actix-files.

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,17 @@
-use axum::{
-    body::StreamBody,
-    extract::Path,
-    http::{header, HeaderMap},
-    response::IntoResponse,
-    routing::get,
-    Router, Server,
-};
-use mimalloc::MiMalloc;
-use tokio_uring::fs::File;
-use std::net::SocketAddr;
-use tokio_util::io::ReaderStream;
+use actix_files::Files;
+use actix_web::{middleware::Logger, App, HttpServer};
 
-#[global_allocator]
-static GLOBAL: MiMalloc = MiMalloc;
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("starting HTTP server at http://localhost:1370");
 
-fn main() {
-    tokio_uring::start(async {
-        let router = Router::new()
-            .route("/sendfile/:req_filename", get(sendfile_api))
-            .into_make_service();
-
-        let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
-        println!("SSL disabled. Listening on {}", addr);
-        Server::bind(&addr)
-            .serve(router)
-            .await
-            .expect("Server startup failed.");
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/sendfile", "../assets/"))
+            // Enable the logger.
+            .wrap(Logger::default())
     })
-}
-
-async fn sendfile_api(Path(req_filename): Path<String>) -> impl IntoResponse {
-    let file = File::open(format!("./assets/{}", req_filename))
-        .await
-        .expect("file not found");
-    let buf = vec![0; 4096];
-    let (_, buf) = file.read_at(buf, 0).await;
-    let mut headers = HeaderMap::new();
-    headers.append(
-        header::CONTENT_TYPE,
-        "application/octet-stream".parse().unwrap(),
-    );
-    headers.append(
-        header::CONTENT_DISPOSITION,
-        format!("attachment; filename=\"{}\"", req_filename)
-            .parse()
-            .unwrap(),
-    );
-    let reader_stream = ReaderStream::new(buf);
-    let body = StreamBody::new(reader_stream);
-    (headers, body)
+    .bind(("127.0.0.1", 1370))?
+    .run()
+    .await
 }
