b45f760e274c5a9cafe180c7b02f8ad24012b886
Ming Chang <mail@mingchang.tw>
2023-03-17 16:52:53 +0800

    tokio_uring: first usable code!!!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use axum::{
-    body::StreamBody,
     extract::Path,
     http::{header, HeaderMap},
     response::IntoResponse,
@@ -7,42 +6,47 @@ use axum::{
     Router, Server,
 };
 use mimalloc::MiMalloc;
-use tokio_uring::fs::File;
 use std::net::SocketAddr;
-use tokio_util::io::ReaderStream;
+use tokio_uring::fs::File;
 
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-fn main() {
-    tokio_uring::start(async {
-        let router = Router::new()
-            .route("/sendfile/:req_filename", get(sendfile_api))
-            .into_make_service();
+#[tokio::main]
+async fn main() {
+    let router = Router::new()
+        .route("/sendfile/:req_filename", get(sendfile_api))
+        .into_make_service();
 
-        let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
-        println!("SSL disabled. Listening on {}", addr);
-        Server::bind(&addr)
-            .serve(router)
-            .await
-            .expect("Server startup failed.");
-        });
+    let addr = SocketAddr::from(([0, 0, 0, 0], 1370));
+    println!("SSL disabled. Listening on {}", addr);
+    Server::bind(&addr)
+        .serve(router)
+        .await
+        .expect("Server startup failed.");
 }
 
 async fn sendfile_api(Path(req_filename): Path<String>) -> impl IntoResponse {
-    let file = File::open(format!("./assets/{}", req_filename)).await.expect("file not found");
-    let mut headers = HeaderMap::new();
-    headers.append(
-        header::CONTENT_TYPE,
-        "application/octet-stream".parse().unwrap(),
-    );
-    headers.append(
-        header::CONTENT_DISPOSITION,
-        format!("attachment; filename=\"{}\"", req_filename)
-            .parse()
-            .unwrap(),
-    );
-    let reader_stream = ReaderStream::new(file);
-    let body = StreamBody::new(reader_stream);
-    (headers, body)
+    tokio::task::block_in_place(move || {
+        tokio_uring::start(async {
+            let file = File::open(format!("./assets/{}", req_filename))
+                .await
+                .expect("file not found");
+            let buffer = vec![0; 104857600];
+            let (_, buf) = file.read_at(buffer, 0).await;
+
+            let mut headers = HeaderMap::new();
+            headers.append(
+                header::CONTENT_TYPE,
+                "application/octet-stream".parse().unwrap(),
+            );
+            headers.append(
+                header::CONTENT_DISPOSITION,
+                format!("attachment; filename=\"{}\"", req_filename)
+                    .parse()
+                    .unwrap(),
+            );
+            (headers, buf)
+        });
+    });
 }
